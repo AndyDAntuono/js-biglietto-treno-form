d4e05056d77ba65816493030296dee83a299c928
AndyDAntuono <andrea.dantuono88@gmail.com>
2024-06-26 19:13:25 +0200

    [UPD] aedded conditional construcution and discount calculation in script.js

--- js/script.js
+++ js/script.js
@@ -3,7 +3,19 @@ btn.addEventListener("click", function (){
     let name_and_surname = document.getElementById("name_and_surname").value;
     let total_km = document.getElementById("total_km").value;
     let age = document.getElementById("age").value;
-    const fullPrice = km * 0.21
+    const fullPrice = total_km * 0.21
+    let discount = 0;
+    
+    if(age === "minor" ) {
+    discount = 20;
+}
 
-    console.log(name_and_surname, total_km, age)
+else if(age === "senior") {
+    discount = 40;
+}
+
+const discountPrice = (fullPrice - (fullPrice * discount / 100)).toFixed(2)
+
+
+    console.log(name_and_surname, total_km, age, discountPrice)
 })
