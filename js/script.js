5d53a871f8e492e7e57bb1265b6d8b4b1c01b76e
AndyDAntuono <andrea.dantuono88@gmail.com>
2024-06-27 01:32:25 +0200

    [UPD] I can't get the desired result when I click on "risultato" button

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
-const btn = document.getElementById("calculate-price");
-btn.addEventListener("click", function (){
-    let name_and_surname = document.getElementById("name_and_surname").value;
+const button = document.getElementById("calculate-price");
+button.addEventListener("click", function () {
+    let firstname_and_surname = document.getElementById("firstname_and_surname").value;
     let total_km = document.getElementById("total_km").value;
     let age = document.getElementById("age").value;
     const fullPrice = total_km * 0.21
@@ -8,14 +8,21 @@ btn.addEventListener("click", function (){
     
     if(age === "minor" ) {
     discount = 20;
-}
+    }
 
-else if(age === "senior") {
+    else if(age === "senior") {
     discount = 40;
-}
+    }
+    
 
-const discountPrice = (fullPrice - (fullPrice * discount / 100)).toFixed(2)
+    const discountPrice = (fullPrice - (fullPrice * discount / 100)).toFixed(2);
+    document.getElementById("firstname_and_surname").innerHTML = firstname_and_surname
+    console.log(firstname_and_surname)
+    console.log(fullPrice)
+
+    document.getElementById("ticket_price").innerText = discountPrice+"€";
+    
 
 
-    console.log(name_and_surname, total_km, age, discountPrice)
+   
 })
