8ac90b2420119f631975da1abd00d2123b58f807
AndyDAntuono <andrea.dantuono88@gmail.com>
2024-06-26 18:56:46 +0200

    [UPD] fixed script.js

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ btn.addEventListener("click", function (){
     let name_and_surname = document.getElementById("name_and_surname").value;
     let total_km = document.getElementById("total_km").value;
     let age = document.getElementById("age").value;
+    const fullPrice = km * 0.21
 
     console.log(name_and_surname, total_km, age)
 })
